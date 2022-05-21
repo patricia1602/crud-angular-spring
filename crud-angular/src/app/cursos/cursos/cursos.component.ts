7776602d566e8bb8bb266edd2644b5f8dbb687f7
Patricia <patyamiga16@hotmail.com>
2022-05-21 18:25:42 -0300

    angular2

--- crud-angular/src/app/cursos/cursos/cursos.component.ts
+++ crud-angular/src/app/cursos/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Curso } from '../model/curso';
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
+  cursos: Curso[] = [
+    { _id: '1s', name: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name','category'];
+
   constructor() { }
+    //this.cursos = [];
 
   ngOnInit(): void {
   }
