d3eada0b4dbc92768bf0a4946cb18bbe0461e66f
Patricia <patyamiga16@hotmail.com>
2022-06-03 14:00:16 -0300

    aula 14 - add projeto spring

--- crud-spring/src/main/java/com/leona/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/leona/crudspring/CrudSpringApplication.java
@@ -1,4 +1,4 @@
-package com.leo;
+package com.leona.crudspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- crud-spring/src/test/java/com/leona/crudspring/CrudSpringApplicationTests.java
+++ crud-spring/src/test/java/com/leona/crudspring/CrudSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.leo;
+package com.leona.crudspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
